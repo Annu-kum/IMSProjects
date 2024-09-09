9b434e967f4d34e5862954b8f269dc4d8c92782c
Annu Kumari <123096055+Annu-kum@users.noreply.github.com>
2024-09-09 16:55:26 +0530

    Update MasterReports.js

--- frontend/vtsfrontend/src/Components/Dashboard/Pages/MastryReport/MasterReports.js
+++ frontend/vtsfrontend/src/Components/Dashboard/Pages/MastryReport/MasterReports.js
@@ -20,7 +20,7 @@ import jspdf from 'jspdf';
 import Header from '../../Header';
 import { toast } from 'react-toastify';
 
-const baseUrl = 'http://127.0.0.1:8000';
+const baseUrl = 'https://ims.digitaaz.com';
 
 export default function MasterReports() {
   const [data, setData] = React.useState([]);
