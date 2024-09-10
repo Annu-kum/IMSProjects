160692e71fc492494fd3f4c263bf7de992cb5793
Annu Kumari <123096055+Annu-kum@users.noreply.github.com>
2024-09-10 12:54:25 +0530

    Update OtrDialog.js

--- frontend/vtsfrontend/src/Components/Dashboard/Pages/OTR/OtrDialog.js
+++ frontend/vtsfrontend/src/Components/Dashboard/Pages/OTR/OtrDialog.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import {Box} from '@mui/material';
 import { Button } from '@mui/material';
 
-const baseUrl = 'https://ims.digitaaz.com';
+const baseUrl = 'https://imsapi.digitaaz.com';
 const ITEM_HEIGHT = 48;
 
 export default function OtrDialog() {
