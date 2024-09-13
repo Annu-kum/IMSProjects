e08d79270797f0969a37898fdefd1b6111b258dd
Annu Kumari <123096055+Annu-kum@users.noreply.github.com>
2024-09-13 14:00:51 +0530

    Update UploadCSV.js

--- frontend/vtsfrontend/src/Components/Dashboard/Pages/Mealers/UploadCSV.js
+++ frontend/vtsfrontend/src/Components/Dashboard/Pages/Mealers/UploadCSV.js
@@ -23,7 +23,7 @@ export default function UploadCSV(props) {
   const[files,setFiles]=React.useState([])
   const[uploadFile,setuploadFile]=React.useState([])
   const FileInputref = React.useRef(null)
-  const sampleFileUrl = 'http://localhost:3000/images/MillersSample.xlsx';
+  const sampleFileUrl = 'https://ims.digitaaz.com/images/MillersSample.xlsx';
 
  
     const downloadSampleFile = async (url) => {
