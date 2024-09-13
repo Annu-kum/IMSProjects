ae105627a04949f588578fa79a0a8fdf61179509
Annu Kumari <123096055+Annu-kum@users.noreply.github.com>
2024-09-13 13:58:34 +0530

    Update UploadEx.js

--- frontend/vtsfrontend/src/Components/Dashboard/Pages/DealerReport/UploadEx.js
+++ frontend/vtsfrontend/src/Components/Dashboard/Pages/DealerReport/UploadEx.js
@@ -23,7 +23,7 @@ export default function UploadEx(props) {
   const[files,setFiles]=React.useState([])
   const[uploadFile,setuploadFile]=React.useState([])
   const FileInputref = React.useRef(null)
-  const sampleFileUrl = 'http://localhost:3000/images/DealersSample.xlsx';
+  const sampleFileUrl = 'https://ims.digitaaz.com/images/DealersSample.xlsx';
 
  
     const downloadSampleFile = async (url) => {
